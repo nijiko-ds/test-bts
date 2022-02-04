a1d362a36336e10e6cb18f3439bc46dbd5e47e80
nijiko-ds <88302871+nijiko-ds@users.noreply.github.com>
2022-02-05 00:55:50 +0700

    Add formbts redux

--- store/store.js
+++ store/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "../slices/counter";
+import formbtsReducer from "../slices/formbts";
 export const store = configureStore({
-  reducer: { counter: counterReducer },
+  reducer: { counter: counterReducer, formbts: formbtsReducer },
   devTools: true,
 });
