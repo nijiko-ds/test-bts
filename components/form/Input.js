970d6cb320d0f0bb767251f3cab5aaebc92e3272
nijiko-ds <88302871+nijiko-ds@users.noreply.github.com>
2022-02-04 16:38:53 +0700

    update form input dan dropdown

--- components/form/Input.js
+++ components/form/Input.js
@@ -9,29 +9,21 @@ const InputComponent = (props) => {
 
   return (
     <div>
-      <Form
-        layout='vertical'
-        form={form}
-        requiredMark={props.required}
-        initialValues={{ layout: "vertical" }}
-        onValuesChange={(e) => console.log("e", e)}
+      <Form.Item
+        label={props.label}
+        required={props.required}
+        tooltip='Input harus diisi'
       >
-        <Form.Item
-          label={props.label}
-          required={props.required}
-          tooltip='Input harus diisi'
-        >
-          <Input
-            placeholder='input placeholder'
-            onChange={(e) => props.setState(e.target.value)}
-            value={props.value}
-            type={props.type}
-            onKeyDown={(e) =>
-              props.type === "number" && e.key === "e" && e.preventDefault()
-            }
-          />
-        </Form.Item>
-      </Form>
+        <Input
+          placeholder='input placeholder'
+          onChange={(e) => props.setState(e.target.value)}
+          value={props.value}
+          type={props.type}
+          onKeyDown={(e) =>
+            props.type === "number" && e.key === "e" && e.preventDefault()
+          }
+        />
+      </Form.Item>
     </div>
   );
 };
