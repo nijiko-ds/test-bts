e24a961d82611c6d6179be2d9a513d319f4bce0e
nijiko-ds <88302871+nijiko-ds@users.noreply.github.com>
2022-02-05 11:40:47 +0700

    Tambah page survey dan profile

---
+++ pages/surveys/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+
+const Surveys = () => {
+  return (
+    <Layout title='Hasil Survey'>
+      <div className='flexboxColCenter h-screen'>
+        <p className='text-3xl'>this is survey</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default Surveys;
